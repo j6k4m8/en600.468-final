e0999e10a90fbdcffd9aade793faf11541ffaee0
Jordan Matelsky <j6k4m8@gmail.com>
2016-04-23 11:52:36 -0400

    Create install-moses.sh

---
+++ scripts/install/install-moses.sh
@@ -0,0 +1,17 @@
+# For general operation
+sudo apt-get install build-essential git-core pkg-config automake libtool wget zlib1g-dev python-dev libbz2-dev
+
+# For regression test suite:
+sudo apt-get install libsoap-lite-perl
+
+# Clone the source
+git clone https://github.com/moses-smt/mosesdecoder.git mosesdecoder
+cd mosesdecoder
+
+# Install boost:
+make -f contrib/Makefiles/install-dependencies.gmake
+
+# Compile moses:
+./compile.sh
+
+cd ~
