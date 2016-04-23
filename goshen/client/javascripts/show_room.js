51fdca0c6dc2954e0a6a6ad6dcd2369b38e524cd
Jordan Matelsky <j6k4m8@gmail.com>
2016-04-23 16:59:54 -0400

    printing text from text chat

--- goshen/client/javascripts/show_room.js
+++ goshen/client/javascripts/show_room.js
@@ -18,13 +18,43 @@ Template.show_room.events({
             var text = ev.target.value;
             if (!text) return;
 
+            var mylang = Session.get('mylang') || 'en';
+
+            var _translations = {};
+            _translations[mylang] = text;
+
             Messages.insert({
                 sender: Session.get('myname') || "Anonymous User",
                 room: Session.get('currentRoom'),
                 text: text,
                 date: new Date(),
-                source: Session.get('mylang') || 'en'
+                source: mylang,
+                translations: _translations
             });
+
+            ev.target.value = "";
         }
     }
-})
+});
+
+Template.show_room.helpers({
+    'messages': function() {
+        return Messages.find({
+            'room': Session.get('currentRoom')
+        }, {
+            sort: { date: 1 }
+        });
+    }
+});
+
+Template._convo_line.helpers({
+    'my_text': function() {
+        var mylang = Session.get('mylang') || 'en';
+
+        if (this.translations[mylang]) {
+            return this.translations[mylang];
+        } else {
+            return "Translation Is Not Implemented Yet"
+        }
+    }
+});
