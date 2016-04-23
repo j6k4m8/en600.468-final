a0ede43bece2ad0a2a00d9a937f7040b97bb774d
Jordan Matelsky <j6k4m8@gmail.com>
2016-04-23 12:57:30 -0400

    add xmlrpc install script

---
+++ scripts/install/install-xmlrpc.sh
@@ -0,0 +1,7 @@
+wget http://downloads.sourceforge.net/project/xmlrpc-c/Xmlrpc-c%20Super%20Stable/1.39.07/xmlrpc-c-1.39.07.tgz?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fxmlrpc-c%2Ffiles%2FXmlrpc-c%2520Super%2520Stable%2F&ts=1461430341&use_mirror=pilotfiber -O xmlrpc-c-1.39.07.tgz
+tar xvf xmlrpc-c-1.39.07.tgz
+cd xmlrpc-c-1.39.07.tgz
+./configure
+make
+sudo su
+make install
