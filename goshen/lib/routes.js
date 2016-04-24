9016c188d691667d4d3f97d71e18ea4def633dfb
Jordan Matelsky <j6k4m8@gmail.com>
2016-04-24 11:54:16 -0400

    routing and room creation

--- goshen/lib/routes.js
+++ goshen/lib/routes.js
@@ -6,8 +6,8 @@ Router.route('/', function () {
     this.render('home', {});
 });
 
-Router.route('/room/:id', function() {
-    this.render('show_room', {
-        data: Rooms.findOne(this.params.id)
-    })
+Router.route('/room/:id', {
+    template: 'show_room',
+    name: 'show_room',
+    data: function() { Session.set('currentRoom', this.params.id); return Rooms.findOne(this.params.id) }
 })
