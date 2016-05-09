c1cde3fbb51fd57cd44384c145f278774d066557
Jordan Matelsky <j6k4m8@gmail.com>
2016-05-09 18:59:11 -0400

    add documentation to the goshenlib js

--- goshenlib/main.js
+++ goshenlib/main.js
@@ -1,7 +1,11 @@
 (function (root) {
 
+    // Append Goshen to the window or root, depending on if we're in a
+    // server or browser environment.
     var _goshen = root._goshen;
 
+    // The languages that moses supports for this example. (Obviously
+    // this can be expanded trivially.)
     LANGUAGES = {
         English: 'en',
         en: 'en',
@@ -9,6 +13,7 @@
         de: 'de'
     }
 
+    // Locales are more specific than languages:
     LOCALES = {
         English: 'en-US',
         en: 'en-US',
@@ -18,6 +23,14 @@
 
 
     serialize = function(obj) {
+        /* Serialize a JSON dictionary into a URL-friendly string.
+
+        Arguments:
+            obj (dict): The object to serialize
+
+        Returns:
+            String: the serialized string.
+        */
         var str = [];
         for (var p in obj) {
             str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
@@ -27,7 +40,9 @@
 
 
     class MosesGoshenAdapter {
+        /* A Moses-mt-server implementation of the GoshenAdapter class. */
         constructor(hostname, protocol, opts) {
+            // Create a new MosesGoshenAdapter.
             this.hostname = hostname;
             this.protocol = protocol || 'http';
         }
@@ -41,13 +56,13 @@
             /* Translate a string `text`, using `opts` as corequisite options.
 
             Arguments:
-            text (str): The text to translate.
-            target (str): The language to translate to
-            source (str): The language to translate from
-            callback (function): The function to call on the translated text
+                text (str): The text to translate.
+                target (str): The language to translate to
+                source (str): The language to translate from
+                callback (function): The function to call on the translated text
 
             Returns:
-            str: The translated text
+                str: The translated text
             */
 
             var requestURL = this.url(serialize({
@@ -76,16 +91,16 @@
         }
     }
 
-
+    // Append to the global namespace.
     _goshen.Goshen = class Goshen {
         constructor(hostname, protocol, type, opts) {
             /* Create a new Goshen object.
 
             Arguments:
-            hostname (str): A protocol-less URI such as `255.255.0.0:3000`
-            protocol (str: 'http'): An http protocol (either 'http' or 'https')
-            type (class): The type of adapter to use by default.
-            opts (dict): Options for configuration.
+                hostname (str): A protocol-less URI such as `255.255.0.0:3000`
+                protocol (str: 'http'): A protocol (either 'http' or 'https')
+                type (class): The type of adapter to use by default.
+                opts (dict): Options for configuration.
 
             The options configuration dictionary can contain
             */
@@ -94,14 +109,31 @@
         }
 
         url(suffix) {
+            /* Generate a URI for a given suffix. Outsources entirely to
+            the GoshenAdapter interface.
+
+            Arguments:
+                suffix (str): The URL to encode
+
+            Returns:
+                str: The complete URL.
+            */
             return this.ga.url(suffix);
         }
 
         translate(text, target, source, callback) {
-            /* Calls the local GoshenAdapter#translate. */
+            /* Translate a string `text`, using `opts` as corequisite options.
+
+            Arguments:
+                text (str): The text to translate.
+                target (str): The language to translate to
+                source (str): The language to translate from
+                callback (function): The fn to call on the translation, if any
+
+            Returns:
+                str: The translated text
+            */
             return this.ga.translate(text, target, source, callback);
         }
-
-
     };
 })(this);
